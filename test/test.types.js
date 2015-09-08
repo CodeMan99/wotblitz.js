ed00b22d0c1b1a787d013ae8e30799ca1fefc255
CodeMan99 <codemister99@yahoo.com>
2015-09-08 09:41:48 -0400

    Update eslintrc.

--- test/test.types.js
+++ test/test.types.js
@@ -3,7 +3,7 @@ var types = require('../lib/types.js');
 
 test('types.numbers', function(t) {
   t.deepEqual(types.numbers('1'), [1], 'parse one element');
-  t.deepEqual(types.numbers('1,2'), [1,2], 'parse two elements');
+  t.deepEqual(types.numbers('1,2'), [1, 2], 'parse two elements');
   t.end();
 });
 

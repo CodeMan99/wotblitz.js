1e12e40b0c9d36621d3d5f03d4fc18a6a8a365b0
CodeMan99 <codemister99@yahoo.com>
2015-09-27 12:34:49 -0400

    Add index.js for the sake of require.
    Currently the npm package is not useful as a dependency.

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  auth: require('./wotblitz-auth'),
+  players: require('./wotblitz-players'),
+  servers: require('./wotblitz-servers'),
+  tankopedia: require('./wotblitz-tankopedia'),
+  'tank-stats': require('./wotblitz-tank-stats')
+};

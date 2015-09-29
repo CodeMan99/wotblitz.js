3dae065f596505fd3b5e39731f362ff703de1fea
CodeMan99 <codemister99@yahoo.com>
2015-09-29 12:39:26 -0400

    Add "clans" command.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   auth: require('./wotblitz-auth'),
+  clans: require('./wotblitz-clans'),
   players: require('./wotblitz-players'),
   servers: require('./wotblitz-servers'),
   tankopedia: require('./wotblitz-tankopedia'),

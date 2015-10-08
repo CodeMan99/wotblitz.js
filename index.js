f00731b0cd2f70e96af98f62dd266b114164ad6f
CodeMan99 <codemister99@yahoo.com>
2015-10-08 17:42:10 -0400

    Export the session, endpoints expect an instance.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   clans: require('./wotblitz-clans'),
   players: require('./wotblitz-players'),
   servers: require('./wotblitz-servers'),
+  session: require('./lib/session.js'),
   tankopedia: require('./wotblitz-tankopedia'),
   tankStats: require('./wotblitz-tank-stats')
 };

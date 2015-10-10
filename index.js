3a575f4c2529b1caf9ea7abb0a2c3660a4e76963
CodeMan99 <codemister99@yahoo.com>
2015-10-10 03:21:11 -0400

    Use env to store the application_id.
    Moving to the env allows for removal of the postinstall
    script, which was honestly a bad hack. This is really a
    hack too, but I can live with this choice as other devs
    can easily provide their own ID at require time.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = {
+var wotblitz = {
   auth: require('./wotblitz-auth'),
   clans: require('./wotblitz-clans'),
   players: require('./wotblitz-players'),
@@ -7,3 +7,12 @@ module.exports = {
   tankopedia: require('./wotblitz-tankopedia'),
   tankStats: require('./wotblitz-tank-stats')
 };
+
+module.exports = process.env.APPLICATION_ID ? wotblitz : setAppId;
+
+function setAppId(applicationId) {
+  if (applicationId) process.env.APPLICATION_ID = applicationId;
+  if (!process.env.APPLICATION_ID) throw new Error('no APPLICATION_ID set in the env');
+
+  return wotblitz;
+}

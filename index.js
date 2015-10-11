45440c2c49dee62147bc8e297e9d029d076eacaf
CodeMan99 <codemister99@yahoo.com>
2015-10-11 07:32:56 -0400

    Use ".js" extension in index.js requires.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var wotblitz = {
-  auth: require('./bin/wotblitz-auth'),
-  clans: require('./bin/wotblitz-clans'),
-  players: require('./bin/wotblitz-players'),
-  servers: require('./bin/wotblitz-servers'),
+  auth: require('./bin/wotblitz-auth.js'),
+  clans: require('./bin/wotblitz-clans.js'),
+  players: require('./bin/wotblitz-players.js'),
+  servers: require('./bin/wotblitz-servers.js'),
   session: require('./lib/session.js'),
-  tankopedia: require('./bin/wotblitz-tankopedia'),
-  tankStats: require('./bin/wotblitz-tank-stats')
+  tankopedia: require('./bin/wotblitz-tankopedia.js'),
+  tankStats: require('./bin/wotblitz-tank-stats.js')
 };
 
 module.exports = process.env.APPLICATION_ID ? wotblitz : setAppId;

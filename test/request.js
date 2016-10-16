21920ff50b4718eab7b67cb1c06bc655b4a6437c
Cody Taylor <codemister99@yahoo.com>
2016-10-16 19:29:48 -0400

    Testing start point. Update dev dependencies.

---
+++ test/request.js
@@ -0,0 +1,8 @@
+var nock = require('nock');
+var test = require('tape');
+
+var request = require('../request.js');
+
+test('request', t => {
+	t.end();
+});
---
+++ test/wotblitz.js
@@ -0,0 +1,9 @@
+var mockery = require('mockery');
+var test = require('tape');
+
+// TODO mock node-fetch
+var wotblitz = require('../wotblitz.js');
+
+test('wotblitz', t => {
+	t.end();
+});
